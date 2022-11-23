2cd9ae8172632219971d7ab32348a3e109586b63
lutfi1661 <lutfi16.mlf@gmail.com>
2022-11-23 17:18:29 +0700

    update

--- models/productModel.js
+++ models/productModel.js
@@ -5,9 +5,9 @@ const productSchema = new mongoose.Schema({
 
     name: { type: String, required: true },
     category: { type: String, required: true },
+    stock: { type: String, required: true },
     price: { type: Number, required: true },
-    image: { type: String, required: true }
-
+    image: { type: String, required: true },
 }, {
     //for date
     timestamps: true
